d870ed5dee992022d8dee3489fbafb0d6f679677
dcronkite <dcronkite@gmail.com>
2021-10-09 10:49:46 -0700

    Added first manifest rx test

--- src/eye_extractor/va/rx.py
+++ src/eye_extractor/va/rx.py
@@ -2,12 +2,12 @@ import re
 
 MANIFEST_PAT = re.compile(
     r'(?:MANIFEST REFRACTION)\W*'
-    r'(?:O\.?D\.?|R\.?E\.?):?\W*(?P<od_sphere>[+-]\d+\.\d+|pl)\W*(?P<od_cylinder>[+-]\d+\.\d+)\W*x\W*(?P<od_axis>\d+)\W*'
-    r'(?:20/\s*(?P<od_denominator>\d+)\s*(?P<od_correct>[+-]\d+(?:\.\d+)?)\s*)?'
+    r'(?:O\.?D\.?|R\.E?\.?):?\W*(?P<od_sphere>[+-]\d+\.\d+|pl)\W*(?P<od_cylinder>[+-]\d+\.\d+)\W*x\W*(?P<od_axis>\d+)\W*'
+    r'(?:20/\s*(?P<od_denominator>\d+)\s*(?P<od_correct>[+-]\d+(?:\.\d+)?)?\s*)?'
     r'(?:add(?:\W*power)?\W*(?P<od_add>\d+(?:\.\d+)?))?'
-    r'(?:O\.?S\.?|L\.?E\.?):?\W*(?P<os_sphere>[+-]\d+\.\d+|pl)\W*(?P<os_cylinder>[+-]\d+\.\d+)\W*x\W*(?P<os_axis>\d+)\W*'
-    r'(?:20/\s*(?P<os_denominator>\d+)\s*(?P<os_correct>[+-]\d+(?:\.\d+)?))?'
-    r'(?:add(?:\W*power)?\W*(?P<os_add>\d+(?:\.\d+)?))?',
+    r'(?:O\.?S\.?|L\.?E?\.?):?\W*(?P<os_sphere>[+-]\d+\.\d+|pl)\W*(?P<os_cylinder>[+-]\d+\.\d+)\W*x\W*(?P<os_axis>\d+)\W*'
+    r'(?:20/\s*(?P<os_denominator>\d+)\s*(?P<os_correct>[+-]\d+(?:\.\d+)?)\W*)?'
+    r'(?:add(?:\W*power)?\W*(?P<os_add>[+-]?\d+(?:\.\d+)?))?',
     re.I
 )
 
@@ -16,16 +16,16 @@ RX_PAT = re.compile(
 )
 
 
-def get_spectacle_prescription(text):
+def get_manifest_rx(text):
     data = {}
     for m in MANIFEST_PAT.finditer(text):
         d = m.groupdict()
         data['manifestrx_sphere_re'] = 0.0 if d['od_sphere'] == 'pl' else float(d['od_sphere'])
         data['manifestrx_cylinder_re'] = float(d['od_cylinder'])
         data['manifestrx_axis_re'] = float(d['od_axis'])
-        data['manifestrx_add_re'] = float(d['od_axis'])
+        data['manifestrx_add_re'] = float(d.get('od_add', 0.0) or 0.0)
         data['manifestrx_sphere_le'] = 0.0 if d['os_sphere'] == 'pl' else float(d['os_sphere'])
         data['manifestrx_cylinder_le'] = float(d['os_cylinder'])
         data['manifestrx_axis_le'] = float(d['os_axis'])
-        data['manifestrx_add_le'] = float(d['os_axis'])
+        data['manifestrx_add_le'] = float(d.get('os_add', 0.0) or 0.0)
     return data
--- test/test_va.py
+++ test/test_va.py
@@ -1,6 +1,7 @@
 import pytest
 
 from eye_extractor.va.extractor2 import vacc_numbercorrect_le
+from eye_extractor.va.rx import get_manifest_rx
 
 
 @pytest.mark.parametrize('text, exp', [
@@ -8,3 +9,22 @@ from eye_extractor.va.extractor2 import vacc_numbercorrect_le
 ])
 def test_vacc_le(text, exp):
     assert vacc_numbercorrect_le(text) == exp
+
+
+@pytest.mark.parametrize(
+    'text, sphere_re, cylinder_re, axis_re, add_re, sphere_le, cylinder_le, axis_le, add_le',
+    [
+        ('''Manifest Refraction: OD: +1.50 -1.00 x 122 20/70 OS: +1.50 -0.25 x 042 20/40+2 Add: +4.25''',
+         1.5, -1, 122, 0,
+         1.5, -0.25, 42, 4.25),
+    ])
+def test_get_manifest_rx(text, sphere_re, cylinder_re, axis_re, add_re, sphere_le, cylinder_le, axis_le, add_le):
+    res = get_manifest_rx(text)
+    assert res['manifestrx_sphere_re'] == sphere_re
+    assert res['manifestrx_cylinder_re'] == cylinder_re
+    assert res['manifestrx_axis_re'] == axis_re
+    assert res['manifestrx_add_re'] == add_re
+    assert res['manifestrx_sphere_le'] == sphere_le
+    assert res['manifestrx_cylinder_le'] == cylinder_le
+    assert res['manifestrx_axis_le'] == axis_le
+    assert res['manifestrx_add_le'] == add_le
