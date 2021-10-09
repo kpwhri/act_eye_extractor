61d5e7db7d8cea3e91110566e458ec8a665cb316
dcronkite <dcronkite@gmail.com>
2021-10-08 19:28:51 -0700

    Fixed additional iop problem

--- test/test_iop.py
+++ test/test_iop.py
@@ -7,8 +7,12 @@ from eye_extractor import iop
     ('TONOMETRY: Tnct  12/19', '12.0', '19.0'),
     ('Tonometry: Method: Ta- Goldman OD: 14 mmHg OS: 15 mmHg', '14.0', '15.0'),
     ('TONOMETRY: deferred to OD', '0', '0'),
-    ('Tonometry; NON CONTACT METHOD:    OD 24    OS 24    mmHg', '24.0', '24.0'),
+    ('TONOMETRY: Tnct  OD Not Assessed  mmHg  OS 11 mmHg', '0', '11.0'),
+    ('Tonometry: Method: NCT    Not assessed in wheel chair', '0', '0'),
+    ('Tonometry; NON CONTACT METHOD:    OD 24    OS 25    mmHg', '24.0', '25.0'),
     ('TONOMETRY:nct 14//15mmHg', '14.0', '15.0'),
+    ('IOP by NCT: see nursing notes  IOP Goldman: R 15mm L 16mm', '15.0', '16.0'),
+    ('IOP by NCT: R 15mm L 16mm', '15.0', '16.0'),
 ])
 def test_iop_measurements(text, re, le):
     assert re == iop.iop_measurement_re(text)
