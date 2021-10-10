15b7ae4ddb21a56d8cacc2d07db771ce5cacf8d4
dcronkite <dcronkite@gmail.com>
2021-10-09 17:34:13 -0700

    Refactored patterns to improve usability/editing

--- src/eye_extractor/va/rx.py
+++ src/eye_extractor/va/rx.py
@@ -1,20 +1,41 @@
 import re
 
-
 sphere = r'([+-]\d+\.\d+|pla?n?o?)'
+cylinder = r'[+-]\d+\.\d+'
+axis = r'\d+'
+denom = r'\d+'
+ncorr = r'[+-]\d+(?:\.\d+)?'
+add = r'\d+(?:\.\d+)?'
+right_eye = r'(?:O\.?D\.?|R\.E?\.?):?'
+left_eye = r'(?:O\.?S\.?|L\.E?\.?):?'
+
+bva = r'(BVA|best correct vision)'
+
+
+def vision_20(denominator_label, ncorrect_label):
+    return rf'(?:20/\s*(?P<{denominator_label}>{denom})\s*(?P<{ncorrect_label}>{ncorr})?\s*)'
+
+
+def prescript(sphere_label, cylinder_label, axis_label):
+    return rf'(?P<{sphere_label}>{sphere})\W*(?P<{cylinder_label}>{cylinder})\W*x\W*(?P<{axis_label}>{axis})'
+
+
+def add_power(add_label):
+    return rf'(?:add(?:\W*power)?\W*(?P<{add_label}>{add})\W*)?'
+
 
 MANIFEST_PAT = re.compile(
-    r'(?:(MANIFEST\W*)?REFRACTION)\W*(manifest\W*)?'
-    r'(?:O\.?D\.?|R\.E?\.?):?\W*(?P<od_sphere>[+-]\d+\.\d+|pl)\W*(?P<od_cylinder>[+-]\d+\.\d+)\W*x\W*(?P<od_axis>\d+)\W*'
-    r'(?:(reading|nv)\W*(?:O\.?D\.?|R\.E?\.?):?\W*(?P<od_sphere_nv>[+-]\d+\.\d+|pl)\W*(?P<od_cylinder_nv>[+-]\d+\.\d+)\W*x\W*(?P<od_axis_nv>\d+)\W*)?'
-    r'(?:20/\s*(?P<od_denominator>\d+)\s*(?P<od_correct>[+-]\d+(?:\.\d+)?)?\s*)?'
-    r'(?:add(?:\W*power)?\W*(?P<od_add>\d+(?:\.\d+)?)\W*)?'
-    r'(?:(BVA|best correct vision)\W*20/\s*(?P<od_denominator_2>\d+)\s*(?P<od_correct_2>[+-]\d+(?:\.\d+)?)?\s*)?'
-    r'(?:O\.?S\.?|L\.?E?\.?):?\W*(?P<os_sphere>[+-]\d+\.\d+|pl)\W*(?P<os_cylinder>[+-]\d+\.\d+)\W*x\W*(?P<os_axis>\d+)\W*'
-    r'(?:(reading|nv)\W*(?:O\.?S\.?|L\.E?\.?):?\W*(?P<os_sphere_nv>[+-]\d+\.\d+|pl)\W*(?P<os_cylinder_nv>[+-]\d+\.\d+)\W*x\W*(?P<os_axis_nv>\d+)\W*)?'
-    r'(?:20/\s*(?P<os_denominator>\d+)\s*(?P<os_correct>[+-]\d+(?:\.\d+)?)\W*)?'
-    r'(?:add(?:\W*power)?\W*(?P<os_add>[+-]?\d+(?:\.\d+)?)\W*)?'
-    r'(?:(BVA|best correct vision)\W*20/\s*(?P<os_denominator_2>\d+)\s*(?P<os_correct_2>[+-]\d+(?:\.\d+)?)?\s*)?',
+    rf'(?:(MANIFEST\W*)?REFRACTION)\W*(manifest\W*)?'
+    rf'{right_eye}\W*{prescript("od_sphere", "od_cylinder", "od_axis")}\W*'
+    rf'(?:(reading|nv)\W*{right_eye}\W*{prescript("od_sphere_nv", "od_cylinder_nv", "od_axis_nv")}\W*)?'
+    rf'{vision_20("od_denominator", "od_correct")}?'
+    rf'{add_power("od_add")}'
+    rf'(?:{bva}\W*{vision_20("od_denominator_2", "od_correct_2")})?'
+    rf'{left_eye}\W*{prescript("os_sphere", "os_cylinder", "os_axis")}\W*'
+    rf'(?:(reading|nv)\W*{left_eye}\W*{prescript("os_sphere_nv", "os_cylinder_nv", "os_axis_nv")}\W*)?'
+    rf'{vision_20("os_denominator", "os_correct")}?'
+    rf'{add_power("os_add")}'
+    rf'(?:{bva}\W*{vision_20("os_denominator_2", "os_correct_2")}?)?',
     re.I
 )
 
