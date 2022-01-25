8eab7b58de863f95e27853cac272906c97d37716
dcronkite <dcronkite@gmail.com>
2022-01-25 11:21:33 -0800

    Jsonl file can be read most recent from directory

--- src/eye_extractor/build_table.py
+++ src/eye_extractor/build_table.py
@@ -17,18 +17,22 @@ def process_data(data):
 
 
 @click.command()
-@click.argument('jsonl_file', type=click.Path(exists=True, dir_okay=False, path_type=pathlib.Path))
+@click.argument('jsonl_file', type=click.Path(exists=True, path_type=pathlib.Path))
 @click.argument('outdir', type=click.Path(file_okay=False, path_type=pathlib.Path))
 def build_table(jsonl_file: pathlib.Path, outdir: pathlib.Path):
     """
 
-    :param jsonl_file:
+    :param jsonl_file: if file, read that file; if directory, get most recent jsonl file
     :param outdir:
     :return:
     """
     now = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
     outdir.mkdir(parents=True, exist_ok=True)
     outpath = outdir / f'variables_{now}.csv'
+    if jsonl_file.is_dir():
+        list_of_paths = jsonl_file.glob('*.jsonl')
+        jsonl_file = max(list_of_paths, key=lambda p: p.stat().st_ctime)
+        print(jsonl_file)
     with (
             open(jsonl_file, encoding='utf8') as fh,
             open(outpath, 'w', encoding='utf8') as out,
