f915bda7e4534646fd40241b04eb9b10353fcba1
dcronkite <dcronkite@gmail.com>
2022-02-23 18:37:58 -0800

    Added additional context

--- test/test_drusen.py
+++ test/test_drusen.py
@@ -4,13 +4,18 @@ from eye_extractor.amd.drusen import find_drusen
 from eye_extractor.laterality import build_laterality_table
 
 
-@pytest.mark.parametrize('text, exp_size, exp_type', [
-    ('WNL OU', None, None),
+@pytest.mark.parametrize('text, exp_size_le, exp_size_re, exp_type_le, exp_type_re', [
+    ('WNL OU', None, None, None, None),
+    ('drusen and RPE drop out OU', 'unknown', 'unknown', 'unknown', 'unknown'),
 ])
-def test_drusen(text, exp_size, exp_type):
+def test_drusen(text, exp_size_le, exp_size_re, exp_type_le, exp_type_re):
     lats = build_laterality_table(text)
     data = find_drusen(text, lats)
-    drusen_size = data.get('drusen_size', None)
-    drusen_type = data.get('drusen_type', None)
-    assert drusen_size == exp_size
-    assert drusen_type == exp_type
+    drusen_size_le = data.get('drusen_size_le', dict())
+    drusen_size_re = data.get('drusen_size_re', dict())
+    drusen_type_le = data.get('drusen_type_le', dict())
+    drusen_type_re = data.get('drusen_type_re', dict())
+    assert drusen_size_le.get('label', None) == exp_size_le
+    assert drusen_size_re.get('label', None) == exp_size_re
+    assert drusen_type_le.get('label', None) == exp_type_le
+    assert drusen_type_re.get('label', None) == exp_type_re
