b43d4c45a3d2c09b8bdabb3e185899dd6526fb7b
dcronkite <dcronkite@gmail.com>
2022-04-28 09:15:23 -0700

    Added negation/rao

--- src/eye_extractor/common/negation.py
+++ src/eye_extractor/common/negation.py
@@ -1,10 +1,12 @@
+import re
 from typing import Match
 
 from eye_extractor.common.string import replace_punctuation
 
 
 def is_negated(m: Match, text: str, terms: set[str],
-               *, word_window: int = 2, char_window: int = 0):
+               *, word_window: int = 2, char_window: int = 0,
+               boundary_chars=':'):
     """
 
     :param m:
@@ -17,6 +19,8 @@ def is_negated(m: Match, text: str, terms: set[str],
     if not char_window:
         char_window = word_window * 10
     context = text[max(0, m.start() - char_window): m.start()]
+    if boundary_chars:
+        context = re.split(f'[{re.escape(boundary_chars)}]', text)[-1]
     no_punct = replace_punctuation(context)
     words = no_punct.split()
     for word in words[-word_window:]:
---
+++ src/eye_extractor/rao/rao.py
@@ -0,0 +1,13 @@
+import re
+
+# RAO, retinal artery occlusion, RvasO (retinal vascular occlusion - can be vein or artery),
+# BRAO, Branch retinal artery occlusion, CRAO, Central retinal artery occlusion
+
+RAO_PAT = re.compile(
+    r'(?:'
+    r'\b[cb]?rao\b'
+    r'|\brvas[o0]\b'
+    r'(?:branch|central)?\W*retinal\W*(artery|vein)?\W*occlu\w+'
+    r''
+    r')'
+)
