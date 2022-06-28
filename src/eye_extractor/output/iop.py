a219eccb2b2dfeb3728854b30c8ab290ff143821
dcronkite <dcronkite@gmail.com>
2022-06-28 16:35:46 -0700

    Fixed iop measurement

--- src/eye_extractor/output/iop.py
+++ src/eye_extractor/output/iop.py
@@ -8,7 +8,7 @@ def iop_measurement(data):
     if not data:
         return {}
     return {
-        'iop_measurement_re': data[0].get('iop_measurement_re'),
-        'iop_measurement_le': data[0].get('iop_measurement_le'),
+        'iop_measurement_re': data[0].get('iop_measurement_re', {}).get('value'),
+        'iop_measurement_le': data[0].get('iop_measurement_le', {}).get('value'),
         'iop_instrument_type': data[0].get('iop_instrument_type'),
     }
