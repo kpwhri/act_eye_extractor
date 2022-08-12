099db85ae711ff64c8359fa1f2a79fe25ca96821
dcronkite <dcronkite@gmail.com>
2022-08-11 18:56:08 -0700

    Extracting RVO kind as well

--- src/eye_extractor/ro/algorithm.py
+++ src/eye_extractor/ro/algorithm.py
@@ -1,9 +1,9 @@
 from eye_extractor.ro.rao import get_rao
-from eye_extractor.ro.rvo import get_rvo
+from eye_extractor.ro.rvo import extract_rvo
 
 
 def extract_ro_variables(text, *, headers=None, lateralities=None):
     return {
         'rao': get_rao(text, headers=headers, lateralities=lateralities),
-        'rvo': get_rvo(text, headers=headers, lateralities=lateralities),
+        'rvo': extract_rvo(text, headers=headers, lateralities=lateralities),
     }
--- test/ro/test_rvo.py
+++ test/ro/test_rvo.py
@@ -1,7 +1,7 @@
 import pytest
 
 from eye_extractor.output.ro import build_rvo
-from eye_extractor.ro.rvo import RVO_PAT, get_rvo
+from eye_extractor.ro.rvo import RVO_PAT, extract_rvo, RvoType, get_rvo_kind
 
 
 @pytest.mark.parametrize('text', [
@@ -11,15 +11,15 @@ from eye_extractor.ro.rvo import RVO_PAT, get_rvo
     'CRVO',
     'central retinal vein occlusion',
 ])
-def test_rao_pattern(text):
+def test_rvo_pattern(text):
     assert RVO_PAT.match(text)
 
 
 @pytest.mark.parametrize('text, exp_value, exp_negword', [
     ('Branch retinal vein occlusion', 1, None),
 ])
-def test_rao_value(text, exp_value, exp_negword):
-    data = get_rvo(text)
+def test_rvo_value(text, exp_value, exp_negword):
+    data = extract_rvo(text)
     assert len(data) > 0
     first_variable = list(data[0].values())[0]
     assert first_variable['value'] == exp_value
@@ -38,3 +38,16 @@ def test_rao_to_column(data, exp_rvo_re, exp_rvo_le):
     result = build_rvo(data)
     assert result['rvo_yesno_re'] == exp_rvo_re
     assert result['rvo_yesno_le'] == exp_rvo_le
+
+
+@pytest.mark.parametrize('text, exp', [
+    ('retinal vein occlusion', RvoType.RVO),
+    ('rvaso', RvoType.RVO),
+    ('BRVO', RvoType.BRVO),
+    ('CRVO', RvoType.CRVO),
+    ('central retinal vein occlusion', RvoType.CRVO),
+])
+def test_rvo_type(text, exp):
+    m = RVO_PAT.search(text)
+    kind = get_rvo_kind(m)
+    assert kind == exp
