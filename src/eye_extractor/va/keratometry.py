bbd5524ae9f2466ac0831e1c5f44fc24de52afa5
dcronkite <dcronkite@gmail.com>
2022-08-16 14:05:22 -0700

    Added text search if not keratometry section

--- src/eye_extractor/va/keratometry.py
+++ src/eye_extractor/va/keratometry.py
@@ -8,18 +8,23 @@ _km_axis = r'(?P<gen_axis#>\d{1,3})'
 _km = fr'{_km_curve}\s*(?:[x@]\s*{_km_axis})?'
 keratometry_pat = rf'(?P<lat#>{laterality_pattern}):\s*{_km.replace("gen", "first")}\W*{_km.replace("gen", "second")}'
 axial_pat = r'(?:iol master|ascans)\W+(?P<al_od>\d{1,2}(?:\.\d{1,2})?)\W+(?P<al_os>\d{1,2}(?:\.\d{1,2})?)'
+_random_text = r'.{1,20}'  # allow notes/obs
 
+KERATOMETRY_HEADER_PAT = re.compile(
+    rf'(?:auto\W*)?keratometry',
+    re.I
+)
 
 KERA_AX_PAT = re.compile(
-    rf'{keratometry_pat.replace("#", "1")}\W*'
-    rf'{keratometry_pat.replace("#", "2")}\W*'
+    rf'{keratometry_pat.replace("#", "1")}{_random_text}'
+    rf'{keratometry_pat.replace("#", "2")}{_random_text}'
     rf'{axial_pat}',
     re.I
 )
 
 KERATOMETRY_PAT = re.compile(
-    rf'{keratometry_pat.replace("#", "1")}\W*'
-    rf'{keratometry_pat.replace("#", "2")}\W*',
+    rf'{keratometry_pat.replace("#", "1")}{_random_text}'
+    rf'{keratometry_pat.replace("#", "2")}',
     re.I
 )
 
@@ -48,58 +53,65 @@ def extract_keratometry(text, *, headers=None, lateralities=None):
         for sect_name, sect_text in headers.iterate(
                 'KERATOMETRY'
         ):  # TODO: other sections?
-            # track where date is: important if analyzing a table
-            has_date_before = False
-            has_date_after = False
-            for pat_label, pat in [
-                ('KERA_AX_PAT', KERA_AX_PAT),
-                ('KERATOMETRY_PAT', KERATOMETRY_PAT),
-            ]:
-                for m in pat.finditer(sect_text):
-                    gd = m.groupdict()
-                    date = None
-                    if not has_date_after:  # don't look before if date already found after
-                        if date := parse_date_before(m, sect_text):  # check before first
-                            has_date_before = True
-                    if not has_date_before:  # don't look after if date already found before
-                        if date := parse_date_before(m, sect_text):
-                            has_date_after = True
-                    lat1 = lat_lookup(m, group='lat1')
-                    lat2 = lat_lookup(m, group='lat2')
-                    od_measures = (
-                        get_by_lat(m, 'first_curve', Laterality.OD, lat1),
-                        get_by_lat(m, 'second_curve', Laterality.OD, lat1),
-                    )
-                    od_axis = get_by_lat(m, 'second_axis', Laterality.OD, lat1)
-                    os_measures = (
-                        get_by_lat(m, 'first_curve', Laterality.OS, lat1),
-                        get_by_lat(m, 'second_curve', Laterality.OS, lat1),
-                    )
-                    os_axis = get_by_lat(m, 'second_axis', Laterality.OS, lat1)
-                    data.append(
-                        {'keratometry': {
-                            'keratometry_flatcurve_re': min(od_measures),
-                            'keratometry_steepcurve_re': max(od_measures),
-                            'keratometry_flataxis_re': (
-                                od_axis if od_measures[0] > od_measures[1] else (od_axis + 90) % 180
-                            ),
-                            'keratometry_steepaxis_re': (
-                                od_axis if od_measures[0] < od_measures[1] else (od_axis + 90) % 180
-                            ),
-                            'keratometry_flatcurve_le': min(os_measures),
-                            'keratometry_steepcurve_le': max(os_measures),
-                            'keratometry_flataxis_le': (
-                                os_axis if os_measures[0] > os_measures[1] else (os_axis + 90) % 180
-                            ),
-                            'keratometry_steepaxis_le': (
-                                os_axis if os_measures[0] < os_measures[1] else (os_axis + 90) % 180
-                            ),
-                            'ax_length_re': float(gd.get('al_od', -1.0)),
-                            'ax_length_le': float(gd.get('al_os', -1.0)),
-                            'term': m.group(),
-                            'regex': pat_label,
-                            'source': sect_name,
-                            'date': date,
-                        }}
-                    )
+            for result in _extract_keratometry(sect_name, sect_text):
+                data.append(result)
+    if not data:
+        for m in KERATOMETRY_HEADER_PAT.finditer(text):
+            for result in _extract_keratometry('ALL', text[m.end():m.end() + 150]):
+                data.append(result)
     return data
+
+
+def _extract_keratometry(sect_name, sect_text):
+    # track where date is: important if analyzing a table
+    has_date_before = False
+    has_date_after = False
+    for pat_label, pat in [
+        ('KERA_AX_PAT', KERA_AX_PAT),
+        ('KERATOMETRY_PAT', KERATOMETRY_PAT),
+    ]:
+        for m in pat.finditer(sect_text):
+            gd = m.groupdict()
+            date = None
+            if not has_date_after:  # don't look before if date already found after
+                if date := parse_date_before(m, sect_text):  # check before first
+                    has_date_before = True
+            if not has_date_before:  # don't look after if date already found before
+                if date := parse_date_before(m, sect_text):
+                    has_date_after = True
+            lat1 = lat_lookup(m, group='lat1')
+            lat2 = lat_lookup(m, group='lat2')
+            od_measures = (
+                get_by_lat(m, 'first_curve', Laterality.OD, lat1),
+                get_by_lat(m, 'second_curve', Laterality.OD, lat1),
+            )
+            od_axis = get_by_lat(m, 'second_axis', Laterality.OD, lat1)
+            os_measures = (
+                get_by_lat(m, 'first_curve', Laterality.OS, lat1),
+                get_by_lat(m, 'second_curve', Laterality.OS, lat1),
+            )
+            os_axis = get_by_lat(m, 'second_axis', Laterality.OS, lat1)
+            yield {'keratometry': {
+                    'keratometry_flatcurve_re': min(od_measures),
+                    'keratometry_steepcurve_re': max(od_measures),
+                    'keratometry_flataxis_re': (
+                        od_axis if od_measures[0] > od_measures[1] else (od_axis + 90) % 180
+                    ),
+                    'keratometry_steepaxis_re': (
+                        od_axis if od_measures[0] < od_measures[1] else (od_axis + 90) % 180
+                    ),
+                    'keratometry_flatcurve_le': min(os_measures),
+                    'keratometry_steepcurve_le': max(os_measures),
+                    'keratometry_flataxis_le': (
+                        os_axis if os_measures[0] > os_measures[1] else (os_axis + 90) % 180
+                    ),
+                    'keratometry_steepaxis_le': (
+                        os_axis if os_measures[0] < os_measures[1] else (os_axis + 90) % 180
+                    ),
+                    'ax_length_re': float(gd.get('al_od', -1.0)),
+                    'ax_length_le': float(gd.get('al_os', -1.0)),
+                    'term': m.group(),
+                    'regex': pat_label,
+                    'source': sect_name,
+                    'date': date,
+                }}
--- test/va/test_keratometry.py
+++ test/va/test_keratometry.py
@@ -32,6 +32,9 @@ def test_keratometry_patterns(pat, text, exp):
         ('', {'KERATOMETRY': 'Auto-Keratometry  OD: 44.00 @ 175 / 45.00 @ 085  OS: 44.75 @ 005 / 46.00 @ 095'},
          44.00, 44.75, 45.00, 46.00, 175, 5, 85, 95, -1, -1,
          ),
+        ('Auto-Keratometry  OD: 44.00 @ 175 / 45.00 @ 085  OS: 44.75 @ 005 / 46.00 @ 095', {},
+         44.00, 44.75, 45.00, 46.00, 175, 5, 85, 95, -1, -1,
+         ),
     ])
 def test_extract_keratometry(
         text, headers, keratometry_flatcurve_re, keratometry_flatcurve_le,
