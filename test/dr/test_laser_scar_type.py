c4401dec8725b0c6453dc42b43c6765a8204b97a
Will <will.i.bowers@kp.org>
2022-08-17 14:41:33 -0700

    W8: Removed panretinal scar tests,  already exists

--- test/dr/test_laser_scar_type.py
+++ test/dr/test_laser_scar_type.py
@@ -7,8 +7,6 @@ from eye_extractor.dr import laser_scar_type as lst
 
 # Test pattern.
 _pattern_cases = [
-    (lst.PANRETINAL_PAT, 'PRP scars', True),
-    (lst.PANRETINAL_PAT, 'PRP marks visible', True),
     (lst.FOCAL_PAT, 'focal laser scars', True),
     (lst.FOCAL_PAT, 'focal tx scars', True),
     (lst.GRID_PAT, 'focal (grid) scars', True),
@@ -29,12 +27,6 @@ def test_laser_scar_type_patterns(pat, text, exp):
 
 
 # Test extract and build.
-_panretinal_extract_and_build_cases = [
-    ('PRP scars OS', {}, -1, 1, -1),
-    ('PRP marks visible OD', {}, 1, -1, -1),
-    ('PRP scarring', {}, -1, -1, 1)
-]
-
 _focal_extract_and_build_cases = [
     ('focal laser scars OU', {}, 1, 1, -1),
     ('focal tx scars OS', {}, -1, 1, -1),
@@ -55,6 +47,37 @@ _macular_extract_and_build_cases = [
     ('macular scars OS.', {}, -1, 1, -1),
     ('macula flat, peripheral laser  OS', {}, -1, 1, -1)
 ]
+
+
+# @pytest.mark.parametrize('text, headers, exp_dr_laser_scar_type_re, exp_dr_laser_scar_type_le, '
+#                          'exp_dr_laser_scar_type_unk',
+#                          _extract_and_build_cases)
+# def test_laser_scar_type_extract_and_build(text,
+#                                            headers,
+#                                            exp_dr_laser_scar_type_re,
+#                                            exp_dr_laser_scar_type_le,
+#                                            exp_dr_laser_scar_type_unk):
+#     pre_json = tx.extract_treatment(text, headers=Headers(headers), lateralities=None)
+#     post_json = json.loads(json.dumps(pre_json))
+#     result = build_lasertype_new(post_json)
+#     assert result['dr_laser_scar_type_re'] == exp_dr_laser_scar_type_re
+#     assert result['dr_laser_scar_type_le'] == exp_dr_laser_scar_type_le
+#     assert result['dr_laser_scar_type_unk'] == exp_dr_laser_scar_type_unk
+
+# @pytest.mark.parametrize('text, headers, exp_dr_laser_scar_type_re, exp_dr_laser_scar_type_le, '
+#                          'exp_dr_laser_scar_type_unk',
+#                          _extract_and_build_cases)
+# def test_laser_scar_type_extract_and_build(text,
+#                                            headers,
+#                                            exp_dr_laser_scar_type_re,
+#                                            exp_dr_laser_scar_type_le,
+#                                            exp_dr_laser_scar_type_unk):
+#     pre_json = tx.extract_treatment(text, headers=Headers(headers), lateralities=None)
+#     post_json = json.loads(json.dumps(pre_json))
+#     result = build_lasertype_new(post_json)
+#     assert result['dr_laser_scar_type_re'] == exp_dr_laser_scar_type_re
+#     assert result['dr_laser_scar_type_le'] == exp_dr_laser_scar_type_le
+#     assert result['dr_laser_scar_type_unk'] == exp_dr_laser_scar_type_unk
 
 
 # @pytest.mark.parametrize('text, headers, exp_dr_laser_scar_type_re, exp_dr_laser_scar_type_le, '
