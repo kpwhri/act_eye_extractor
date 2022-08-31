5b902fee73d6db87c95d088fc4d9db9b6aeac04d
Will <will.i.bowers@kp.org>
2022-08-31 10:20:48 -0700

    W8: Added venous beading extract & build test

--- src/eye_extractor/dr/venous_beading.py
+++ src/eye_extractor/dr/venous_beading.py
@@ -11,3 +11,16 @@ VEN_BEADING_PAT = re.compile(
     r')\b',
     re.I
 )
+
+
+def get_ven_beading(text: str, *, headers=None, lateralities=None) -> list:
+    # if not lateralities:
+    #     lateralities = build_laterality_table(text)
+    # data = []
+    # for new_var in _get_hemorrhage_type(text, lateralities, 'ALL'):
+    #     data.append(new_var)
+    # if headers:
+    #     pass
+    #
+    # return data
+    pass
--- test/dr/test_venous_beading.py
+++ test/dr/test_venous_beading.py
@@ -1,7 +1,7 @@
 import json
 import pytest
 
-from eye_extractor.dr.venous_beading import VEN_BEADING_PAT
+from eye_extractor.dr.venous_beading import get_ven_beading, VEN_BEADING_PAT
 from eye_extractor.output.dr import build_ven_beading
 
 # Test pattern.
@@ -20,3 +20,33 @@ def _get_pattern_cases():
 def test_ven_beading_patterns(pat, text, exp):
     m = pat.search(text)
     assert bool(m) == exp
+
+
+# Test extract and build.
+_ven_beading_extract_and_build_cases = [
+    ('mild VB OU', {}, 'MILD', 'MILD', 'UNKNOWN'),
+    ('Mild - moderate venous beading OD', {}, 'MODERATE', 'UNKNOWN', 'UNKNOWN'),
+    ('no venous beading ou', {}, 'NONE', 'NONE', 'UNKNOWN'),
+    ('moderate VB OS', {}, 'UNKNOWN', 'MODERATE', 'UNKNOWN'),
+    ('severe venous beading', {}, 'UNKNOWN', 'UNKNOWN', 'SEVERE'),
+    ('VB severity=3Q OS', {}, 'UNKNOWN', 'Q3', 'UNKNOWN'),
+    # TODO: `get_laterality_for_term` returning unknown instead of OD
+    ('venous beading temporal and inferior quadrant OD', {}, 'Q2', 'UNKNOWN', 'UNKNOWN'),
+    ('nasal quadrant, VB', {}, 'UNKNOWN', 'UNKNOWN', 'Q1'),
+    ('VB in all quadrants ou', {}, 'Q4', 'Q4', 'UNKNOWN'),
+]
+
+
+@pytest.mark.parametrize('text, headers, exp_venbeading_re, exp_venbeading_le, exp_venbeading_unk',
+                         _ven_beading_extract_and_build_cases)
+def test_ven_beading_extract_and_build(text,
+                                       headers,
+                                       exp_venbeading_re,
+                                       exp_venbeading_le,
+                                       exp_venbeading_unk):
+    pre_json = get_ven_beading(text)
+    post_json = json.loads(json.dumps(pre_json))
+    result = build_ven_beading(post_json)
+    assert result['venbeading_re'] == exp_venbeading_re
+    assert result['venbeading_le'] == exp_venbeading_le
+    assert result['venbeading_unk'] == exp_venbeading_unk
