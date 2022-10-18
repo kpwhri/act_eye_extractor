f9b3184104ac542668ae02d3b9a9361ecc2efff1
dcronkite <dcronkite@gmail.com>
2022-10-18 11:08:38 -0700

    Fixed bug assigning tuple to fluid variable

--- src/eye_extractor/common/algo/extract.py
+++ src/eye_extractor/common/algo/extract.py
@@ -5,5 +5,5 @@ from eye_extractor.common.algo.treatment import extract_treatment
 def extract_common_algorithms(text, *, headers=None, lateralities=None):
     data = {}
     data['treatment'] = extract_treatment(text, headers=headers, lateralities=lateralities)
-    data['fluid'] = extract_fluid(text, headers=headers, lateralities=lateralities),
+    data['fluid'] = extract_fluid(text, headers=headers, lateralities=lateralities)
     return data
