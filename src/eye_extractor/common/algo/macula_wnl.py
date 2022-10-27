c72261f20b19a3727f9e7fedd238ab68461c53fa
dcronkite <dcronkite@gmail.com>
2022-10-27 10:00:39 -0700

    Removed print statement debugging

--- src/eye_extractor/common/algo/macula_wnl.py
+++ src/eye_extractor/common/algo/macula_wnl.py
@@ -15,10 +15,7 @@ def extract_macula_wnl(text, headers=None, lateralities=None):
     """Check for cases when macula is normal. This effects AMD, DR, and RO."""
     if headers:
         for section_header, section_text in headers.iterate('MACULA', 'MAC'):
-            print(section_text)
             for m in MACULA_WNL.finditer(section_text):
-                print(m)
-                print(is_negated(m, section_text))
                 if not is_negated(m, section_text):
                     return {
                         'date': parse_nearest_date_to_line_start(m.start(), text),
