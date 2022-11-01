f3c2dfb83e16dfd316461c0474303ae9dded55dc
dcronkite <dcronkite@gmail.com>
2022-11-01 11:37:04 -0700

    Added untested functions for removing history sections

---
+++ src/eye_extractor/sections/history.py
@@ -0,0 +1,76 @@
+import re
+
+from eye_extractor.sections.utils import get_index_of_next_section_start
+
+history = r'(?:hist(ory)?|hx)'
+medical = r'(?:ocular|eye|optical|med(?:ical)?)'
+
+HISTORY_SECTION_PAT = re.compile(
+    rf'\b(?:'
+    rf'review\s*of\s*symptoms'
+    rf'|social\s*{history}'
+    rf'|(?:past\s*)?medical\s*{history}(?:\s*of)?'
+    rf'|pm\s*{history}(?:\s*of)?'
+    rf'|fam(?:ily)?\s*{history}(?:\s*of)?'
+    rf'|past\s*(?:\w+\W*){{,6}}'
+    rf')[:-]',
+    re.I
+)
+
+PROBLEM_LIST = re.compile(
+    rf'(?:'
+    rf'(?P:patient\s*)?active\s*problem\s*list'
+    rf')',
+    re.I
+)
+
+STOP_BEFORE_REGEX = re.compile(
+    rf'(?:'
+    rf'signed\s*by'
+    rf'|date'
+    rf'|time'
+    rf'|(?P:patient\s*)?active\s*problem\s*list'
+    rf')',
+    re.I
+)
+
+
+def _find_history_sections(text):
+    """
+    Retrieve offsets for history sections of note.
+    :param text:
+    :return: yields matched_section_name, start_index (incl header), start_index, end_index
+    """
+    for m in HISTORY_SECTION_PAT.finditer(text):
+        end_index = get_index_of_next_section_start(
+            text, m.end(), max_length=1000, stop_before_regex=STOP_BEFORE_REGEX
+        )
+        yield m.group().strip().strip(':'), m.start(), m.end(), end_index
+
+
+def retrieve_history_sections(text):
+    """
+    Retrieve history sections as dicts
+    :param text:
+    :return: yields {name: section name, text: text in section (not including header value)}
+    """
+    for section_name, _, start_index, end_index in _find_history_sections(text):
+        yield {
+            'name': section_name,
+            'text': text[start_index: end_index]
+        }
+
+
+def remove_history_sections(text):
+    """
+    Remove entire history sections from text.
+    :param text:
+    :return:
+    """
+    results = []
+    prev_end = 0
+    for _, start_index, _, end_index in _find_history_sections(text):
+        results.append(text[prev_end: start_index])
+        prev_end = end_index
+    results.append(text[prev_end:])
+    return ''.join(results)
---
+++ src/eye_extractor/sections/utils.py
@@ -0,0 +1,35 @@
+import re
+
+from eye_extractor.nlp.character_groups import LINE_START_CHARS_RX
+
+NEWLINE_SECTION_PAT = re.compile(
+    rf'(?:[{LINE_START_CHARS_RX}]|^)(?P<header>[ \t\w/\-.]+)[:-]\W',
+)
+
+SKIP_SECTIONS = frozenset({'OD', 'OS'})
+
+
+def get_index_of_next_section_start(text, start_index, *, max_length=None,
+                                    stop_before_regex=None, skip_sections=SKIP_SECTIONS):
+    """
+    Find the start of the next section.
+    :param text:
+    :param start_index:
+    :param max_length:
+    :param stop_before_regex:
+    :param skip_sections:
+    :return:
+    """
+    if stop_before_regex:  # check for end boundary
+        if m := stop_before_regex.search(text[start_index:]):
+            if not max_length or m.start() < max_length:
+                max_length = m.start()
+    # look for next section
+    for m in NEWLINE_SECTION_PAT.finditer(text, pos=start_index):
+        if max_length and m.start() - start_index > max_length:
+            return max_length
+        section_name = m.group('header').strip().upper()
+        if section_name in skip_sections:
+            continue
+        else:
+            return m.start()  # return start of entire pattern
