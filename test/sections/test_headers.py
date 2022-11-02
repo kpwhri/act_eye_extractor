017af894a7a44592be60cb17740e2963ec5d73f0
dcronkite <dcronkite@gmail.com>
2022-11-02 09:11:56 -0700

    Added tests for a section that was not found

--- test/sections/test_headers.py
+++ test/sections/test_headers.py
@@ -5,17 +5,18 @@ def test_headers_iterate():
     text = 'MAC: fluid od'
     section = Headers({'MACULA': 'fluid os'})
     section.set_text(text)
-    sections = list(section.iterate('MACULA', 'MAC'))
+    sections = list(section.iterate('MACULA', 'MAC', 'AMD'))
     # test header state
     assert len(section.data) == 1  # only 1 dict
     assert len(section.text) == len(text)  # stored text correctly
-    assert len(section.searched) == 1  # added 'MAC'
+    assert len(section.searched) == 2  # added 'MAC' and 'AMD'
     assert 'MAC' in section.searched.keys()
     assert section.searched['MAC'].strip() == 'fluid od'
+    assert 'AMD' in section.searched.keys()
+    assert section.searched['AMD'] is None
     # test return results
+    assert len(sections) == 2
     assert sections[0][0] == 'MACULA'
     assert sections[0][1].strip() == 'fluid os'
     assert sections[1][0] == 'MAC'
     assert sections[1][1].strip() == 'fluid od'
-
-
