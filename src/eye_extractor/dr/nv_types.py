d46b4b87e2361e5f5c0a61f1b0f8cf9f3f9af363
Will <will.i.bowers@kp.org>
2023-09-29 16:21:26 -0700

    W8: Passing neovasc tests.

--- src/eye_extractor/dr/nv_types.py
+++ src/eye_extractor/dr/nv_types.py
@@ -4,6 +4,12 @@ from eye_extractor.common.get_variable import get_variable
 from eye_extractor.nlp.negate.negation import is_negated, is_post_negated, has_after
 from eye_extractor.laterality import build_laterality_table, create_new_variable
 
+NV_PAT = re.compile(
+    r'\b('
+    r'neovascularization'
+    r'\b)',
+    re.I
+)
 NVA_PAT = re.compile(
     r'\b('
     r'(angular\s+)neovascularization\s+of(\s+the)?\s+angle'
@@ -45,7 +51,32 @@ def get_neovasc(text: str, *, headers=None, lateralities=None) -> list:
 
 
 def _get_neovasc(text: str, lateralities, source: str) -> dict:
-    pass
+    for pat_label, pat in [
+        ('NV_PAT', NV_PAT),
+        ('NVA_PAT', NVA_PAT),
+        ('NVI_PAT', NVI_PAT),
+        ('NVD_PAT', NVD_PAT),
+        ('NVE_PAT', NVE_PAT),
+    ]:
+        for m in pat.finditer(text):
+            negated = (
+                is_negated(m, text, word_window=4)
+                or is_negated(m, text, terms={'no'}, word_window=8)
+                or is_post_negated(m, text, terms={'normal', 'neg', 'deferred', 'no', 'nc'}, word_window=2)
+            )
+            if has_after(m if isinstance(m, int) else m.start(),
+                         text,
+                         terms={'decreased'},
+                         word_window=3):
+                break
+            yield create_new_variable(text, m, lateralities, 'neovasc_yesno', {
+                'value': 0 if negated else 1,
+                'term': m.group(),
+                'label': 'no' if negated else 'yes',
+                'negated': negated,
+                'regex': pat_label,
+                'source': source
+            })
 
 
 def get_nv_types(text: str, *, headers=None, lateralities=None) -> list:
@@ -83,6 +114,6 @@ def _get_nv_types(text: str, lateralities):
                 'term': m.group(),
                 'label': 'no' if negated else 'yes',
                 'negated': negated,
-                'regex': f'{variable}_PAT',
+                'regex': pat_label,
                 'source': 'ALL'
             })
