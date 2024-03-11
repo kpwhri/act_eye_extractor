8a5b2335e08e31bdda2cc4a6c7aac337707ac075
Will <will.i.bowers@kp.org>
2024-03-11 16:57:36 -0700

    W8: Updated hemorrhage type YES_NOS patterns and hemorrhage DR patterns with existing patterns.

--- src/eye_extractor/dr/dr_type.py
+++ src/eye_extractor/dr/dr_type.py
@@ -1,7 +1,7 @@
 import enum
 import re
 
-from eye_extractor.dr.dr_yesno import DR_YESNO_PAT
+from eye_extractor.dr.dr_yesno import DR_YESNO_PAT, DR_YESNO_ABBR_PAT, DR_YESNO_NEG_PAT
 from eye_extractor.nlp.negate.negation import has_before, has_after, is_negated, is_post_negated
 from eye_extractor.common.severity import extract_risk, extract_severity, Risk, Severity
 from eye_extractor.laterality import build_laterality_table, create_new_variable
@@ -29,20 +29,6 @@ PDR_PAT = re.compile(
     r')\b',
     re.I
 )
-# TODO: Replace with `DR_YESNO_PAT`
-DR_NOS_PAT = re.compile(
-    r'\b('
-    r'diabetic\s+retinopathy'
-    r')\b',
-    re.I
-)
-# TODO: Replace with `DR_YESNO_ABBR_PAT`
-# Separate pattern to capture case-sensitive abbreviations.
-DR_NOS_ABBR_PAT = re.compile(
-    r'\b('
-    r'DR|dr'
-    r')\b',
-)
 
 
 def get_dr_type(text: str, *, headers=None, lateralities=None) -> list:
@@ -62,8 +48,9 @@ def _get_dr_type(text: str, lateralities, source: str) -> dict:
     for pat_label, pat, dr_type, dr_label, sev_var in [
         ('NPDR_PAT', NPDR_PAT, DrType.NPDR, 'nonproliferative diabetic retinopathy', 'nonprolifdr'),
         ('PDR_PAT', PDR_PAT, DrType.PDR, 'proliferative diabetic retinopathy', None),
-        ('DR_NOS_PAT', DR_NOS_PAT, DrType.YES_NOS, 'diabetic retinopathy', None),
-        ('DR_NOS_ABBR_PAT', DR_NOS_ABBR_PAT, DrType.YES_NOS, 'diabetic retinopathy', None),
+        ('DR_YESNO_NEG_PAT', DR_YESNO_NEG_PAT, DrType.YES_NOS, 'diabetic retinopathy', None),
+        ('DR_YESNO_PAT', DR_YESNO_PAT, DrType.YES_NOS, 'diabetic retinopathy', None),
+        ('DR_YESNO_ABBR_PAT', DR_YESNO_ABBR_PAT, DrType.YES_NOS, 'diabetic retinopathy', None),
     ]:
 
         for m in pat.finditer(text):
@@ -71,19 +58,19 @@ def _get_dr_type(text: str, lateralities, source: str) -> dict:
                           text,
                           terms={'confirm'},
                           word_window=2):
-                break
+                continue
             if dr_type is DrType.YES_NOS:
                 if has_before(m if isinstance(m, int) else m.start(),
                               text,
                               terms={'confirm', 'surgeon', 'tablet', 'exam'},
                               word_window=2,
                               boundary_chars='¶'):
-                    break
+                    continue
                 elif has_after(m if isinstance(m, int) else m.start(),
                                text,
                                terms={'exam'},
                                word_window=6):
-                    break
+                    continue
             negated = (
                 is_negated(m, text, word_window=3)
                 or is_post_negated(m, text, terms={'no'}, word_window=3)
@@ -169,7 +156,7 @@ def _get_pdr(text: str, lateralities, source: str) -> dict:
                       text,
                       terms={'confirm'},
                       word_window=2):
-            break
+            continue
         if risks:
             for risk in risks:
                 yield create_new_variable(text, m, lateralities, 'prolifdr', {
