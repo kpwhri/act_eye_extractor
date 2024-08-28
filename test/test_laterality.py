24e0f7868364880e9c6cdc7a66dcec1588de88d9
Will <106627714+bowers-will@users.noreply.github.com>
2024-08-28 14:06:55 -0700

    W8: Updated 'get_by_index' and sub-functions to use a regex match span vs. match start.

--- test/test_laterality.py
+++ test/test_laterality.py
@@ -3,29 +3,29 @@ import pytest
 from eye_extractor.laterality import Laterality, build_laterality_table, LATERALITY_PATTERN
 
 
-@pytest.mark.parametrize('text, start_index, exp', [
-    ('IOL OD: 1+ PCO IOL OS: tr pco', 9, Laterality.OD),
-    ('IOL OD: 1+ PCO IOL OS: tr pco', 23, Laterality.OS),
-    ('PCIOL od, ac iol os', 0, Laterality.OD),
-    ('PCIOL od, ac iol os', 10, Laterality.OS),
-    ('dot blot hemorrhage temporal and inferior quadrant OD', 0, Laterality.OD),
-    ('intermediate drusen od, heavy drusen os', 0, Laterality.OD),
-    ('intermediate drusen od, heavy drusen os', 30, Laterality.OS),
-    ('OD: intermediate drusen OS: heavy drusen', 0, Laterality.OD),
-    ('OD: intermediate drusen OS: heavy drusen', 30, Laterality.OS),
-    ('AMD od > os', 3, Laterality.OU),
-    ('AMD od>os', 3, Laterality.OU),
-    ('AMD od  >  os', 3, Laterality.OU),
-    ('Gonioscopy: right eye: no NVA left eye: no NVA', 26, Laterality.OD),
-    ('Gonioscopy: right eye: no NVA left eye: no NVA', 43, Laterality.OS),
-    ('CMT OD:200 OS:200', 8, Laterality.OD),
-    ('CMT OD:200 possible epiretinal membrane OS:200 possible epiretinal membrane', 27, Laterality.OD),
-    ('CMT OD:200 OS:200', 15, Laterality.OS),
-    ('CMT OD:200 possible epiretinal membrane OS:200 possible epiretinal membrane', 60, Laterality.OS),
+@pytest.mark.parametrize('text, match_span, exp', [
+    ('IOL OD: 1+ PCO IOL OS: tr pco', (9, 18), Laterality.OD),
+    ('IOL OD: 1+ PCO IOL OS: tr pco', (23, 29), Laterality.OS),
+    ('PCIOL od, ac iol os', (0, 6), Laterality.OD),
+    ('PCIOL od, ac iol os', (10, 16), Laterality.OS),
+    ('dot blot hemorrhage temporal and inferior quadrant OD', (0, 19), Laterality.OD),
+    ('intermediate drusen od, heavy drusen os', (0, 22), Laterality.OD),
+    ('intermediate drusen od, heavy drusen os', (24, 36), Laterality.OS),
+    ('OD: intermediate drusen OS: heavy drusen', (4, 23), Laterality.OD),
+    ('OD: intermediate drusen OS: heavy drusen', (28, 40), Laterality.OS),
+    ('AMD od > os', (0, 3), Laterality.OU),
+    ('AMD od>os', (0, 3), Laterality.OU),
+    ('AMD od  >  os', (0, 3), Laterality.OU),
+    ('Gonioscopy: right eye: no NVA left eye: no NVA', (26, 29), Laterality.OD),
+    ('Gonioscopy: right eye: no NVA left eye: no NVA', (43, 46), Laterality.OS),
+    ('CMT OD:200 OS:200', (8, 10), Laterality.OD),
+    ('CMT OD:200 possible epiretinal membrane OS:200 possible epiretinal membrane', (27, 40), Laterality.OD),
+    ('CMT OD:200 OS:200', (11, 17), Laterality.OS),
+    ('CMT OD:200 possible epiretinal membrane OS:200 possible epiretinal membrane', (40, 46), Laterality.OS),
 ])
-def test_laterality_by_index(text, start_index, exp):
+def test_laterality_by_index(text, match_span, exp):
     latloc = build_laterality_table(text)
-    res = latloc.get_by_index(start_index, text)
+    res = latloc.get_by_index(match_span, text)
     assert res == exp
 
 
